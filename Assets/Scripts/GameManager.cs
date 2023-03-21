a70983b400d8e0bbcce8382952b476cc90a7da53
BeerHuntor <rt001a9138@gmail.com>
2023-03-21 18:24:41 +0000

    Midway through refactoring code to condense code, and switch things around in the classes.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,26 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
+    //Static instances
     public static GameManager instance;
+
+    //References
+    [SerializeField] private List<Transform> pickupPoints;
+    [SerializeField] private List<Transform> destinations;
+
+    //Local Variables
     [SerializeField] private bool hasPassenger;
     [SerializeField] private bool isPassengerWaiting;
-    private bool startOfGame; 
-    void Awake()
-    {
+
+    private Transform currentPickupPoint;
+    private Transform currentDestination;
+
+    private bool startOfGame;
+    void Awake() {
         instance = this;
     }
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         InitGame();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
     }
+
+    public Transform GetRandomPickupPointTransform() {
+        int index = Random.Range(0, pickupPoints.Count);
+        currentPickupPoint = pickupPoints[index].transform;
+        return currentPickupPoint;
+    }
+
+    private void ClearCurrentPickupPoint() {
+        currentPickupPoint = null; 
+    }
+    private Transform GetCurrentPickupPoint() {
+        return currentPickupPoint;
+    }
+
+    private Transform GetRandomDestinationTransform() {
+        int index = Random.Range(0, destinations.Count);
+        currentDestination = destinations[index].transform;
+        return currentDestination;
+    }
+
     public void InitGame()
     {
         hasPassenger = false;
