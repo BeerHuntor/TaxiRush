6f21f0b2c70857b0628b44758777be1498bce09e
BeerHuntor <rt001a9138@gmail.com>
2023-03-22 13:51:20 +0000

    Refactored the Player.cs to handle the new input system. It also has the functionality to accept a passenger object which it then calls the passenger methods withing Passenger.cs - Had an issue where when the passenger disembarked it would automatically reembark due to the way the code its using the OnTriggerEnter2D - to solve this I added a check to see if the passenger the trigger is entering is equal to the last passenger, if so don't do anything. This solved the issue.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Net.Sockets;
@@ -5,9 +6,16 @@ using System.Security.Cryptography;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+
+    public event EventHandler OnPassengerEmbark;
+    public event EventHandler OnPassengerDisembark; 
+    
     //References
     [SerializeField] GameInputManager gameInputManager;
 
+    private Passenger passenger;
+    private Passenger lastPassenger;
+    
     //Components
     private Rigidbody2D rb2D; 
 
@@ -36,9 +44,50 @@ public class Player : MonoBehaviour {
 
         }
 
-        transform.Translate(Vector3.up * inputVector.y * accelerationFactor * Time.deltaTime);
-
+        transform.Translate(Vector3.up * (inputVector.y * accelerationFactor * Time.deltaTime));
     }
 
+    public void ClearPassenger() {
+        passenger = null;
+    }
 
+    public bool HasPassenger() {
+        if (passenger != null) {
+            return true; 
+        }
+        return false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D trigger) {
+        if (trigger.TryGetComponent(out Passenger passenger)) {
+            //Check if the current triggered component is the same passenger as we had before. If it is, don't do anything. 
+            if (lastPassenger == passenger) {
+                return;
+            }
+            // We have enetered a passengers trigger. 
+            passenger.SetParent(this.gameObject);
+            
+            this.passenger = passenger;
+            lastPassenger = passenger;
+            
+            passenger.Hide();
+            
+            if (OnPassengerEmbark != null) {
+                OnPassengerEmbark.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        if (trigger.TryGetComponent(out Destination destination)) {
+            //We have entered a destination trigger
+            if (destination.gameObject.transform == GameManager.instance.GetCurrentDestination()) {
+                //We have entered the current destination trigger.
+                if (OnPassengerDisembark != null) {
+                    OnPassengerDisembark.Invoke(this, EventArgs.Empty);
+                } 
+                this.passenger.SetParent(destination.gameObject);
+                this.passenger.Show();
+                ClearPassenger();
+            }
+        }
+    }
 }
